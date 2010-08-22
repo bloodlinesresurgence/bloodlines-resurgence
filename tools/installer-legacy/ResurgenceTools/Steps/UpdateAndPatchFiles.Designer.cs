c321d23b186a55b090a785b16f24ce60ea68a042
Daedalus Raistlin <daedalus.raistlin@gmail.com>
2010-08-23 01:44:58 +1000

    Add UpdateAndPatchFiles

---
+++ tools/installer-legacy/ResurgenceTools/Steps/UpdateAndPatchFiles.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace ResurgenceTools.Steps
+{
+    partial class UpdateAndPatchFiles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ContentPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // WizardDescription
+            // 
+            this.WizardDescription.Size = new System.Drawing.Size(137, 13);
+            this.WizardDescription.Text = "Update to the latest version";
+            // 
+            // UpdateAndPatchFiles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 350);
+            this.Name = "UpdateAndPatchFiles";
+            this.Text = "UpdateAndPatchFiles";
+            this.ContentPanel.ResumeLayout(false);
+            this.ContentPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ tools/installer-legacy/ResurgenceTools/Steps/UpdateAndPatchFiles.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ResurgenceTools;
+using ResurgenceLib;
+using System.Diagnostics;
+
+namespace ResurgenceTools.Steps
+{
+    internal partial class UpdateAndPatchFiles : GenericRunProcess
+    {
+        public UpdateAndPatchFiles()
+        {
+            InitializeComponent();
+        }
+
+        public UpdateAndPatchFiles(TranslationProvider Provider)
+            : base(Provider)
+        {
+        }
+
+        protected override void DoInitializeComponent()
+        {
+            base.DoInitializeComponent();
+            InitializeComponent();
+        }
+
+        protected override Result DoWork()
+        {
+            prepareSmallgit();
+
+            // Stage 1: git stash
+            //          Store changes to tracked files
+            AppendText("Saving any changes...");
+            git("stash");
+
+            // Stage 2: git pull
+            //          Download and apply any changes
+            AppendText("Downloading and applying changes...");
+            git("pull");
+
+            // Stage 3: git stash pop
+            //          Retrieve stored changes and reapply them
+            AppendText("Restoring changes made prior to update");
+            git("stash pop");
+
+            return Result.Success;
+        }
+
+        private void git(string p)
+        {
+            ProcessStartInfo info = new ProcessStartInfo(Program.Settings.Tools_Directory + "\\smallgit\\bin\\git.exe", p);
+            info.WorkingDirectory = Program.Settings.DestinationDirectory;
+            info.EnvironmentVariables["PATH"] = Program.Settings.Tools_Directory + "\\smallgit\\bin";
+            info.EnvironmentVariables["HOME"] = Program.Settings.Tools_Directory + "\\smallgit";
+
+            RunProcess(info);
+        }
+
+        private void prepareSmallgit()
+        {
+            AppendText("Preparing updater...");//TranslationProvider.Translate("!StageOne", this.Name));
+
+            ProcessStartInfo info = new ProcessStartInfo(Program.Settings.Tools_Directory + "\\Unrar.exe",
+                String.Format(Program.Settings.Generic_Unrar_Arguments, Program.Settings.smallgit_Rar));
+            info.WorkingDirectory = Program.Settings.Tools_Directory;
+
+            RunProcess(info);
+
+            AppendText(TranslationProvider.Translate("!StageOneDone", this.Name) + "\n");
+        }
+    }
+}
