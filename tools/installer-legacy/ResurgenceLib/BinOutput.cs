3b02fef95c12dee63364d83b24b60d747561a0e5
Daedalus Raistlin <daedalus.raistlin@gmail.com>
2010-08-27 00:10:06 +1000

    Add BinOutput struct

---
+++ tools/installer-legacy/ResurgenceLib/BinOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RevivalLib
+{
+    /// <summary>
+    /// Output from executing a binary.
+    /// </summary>
+    public struct BinOutput
+    {
+        public string StdOutput;
+        public string StdError;
+    }
+}
