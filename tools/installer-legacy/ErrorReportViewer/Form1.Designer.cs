797f719fa07f2998211c5e27de41b9dec3ee90f5
Daedalus Raistlin <daedalus.raistlin@gmail.com>
2010-09-02 01:03:13 +1000

    Added Error Report Viewer

---
+++ tools/installer-legacy/ErrorReportViewer/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ErrorReportViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Report = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ReportNumber = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.ReportNumber);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(572, 40);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Report);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(572, 217);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Error Report Contents:";
+            // 
+            // Report
+            // 
+            this.Report.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Report.Location = new System.Drawing.Point(3, 16);
+            this.Report.Name = "Report";
+            this.Report.Size = new System.Drawing.Size(566, 198);
+            this.Report.TabIndex = 0;
+            this.Report.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Report Number:";
+            // 
+            // ReportNumber
+            // 
+            this.ReportNumber.Location = new System.Drawing.Point(101, 10);
+            this.ReportNumber.Name = "ReportNumber";
+            this.ReportNumber.Size = new System.Drawing.Size(100, 20);
+            this.ReportNumber.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get Report";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 257);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Error Log Viewer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ReportNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox Report;
+    }
+}
+
---
+++ tools/installer-legacy/ErrorReportViewer/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ErrorReportViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Collections.Specialized.NameValueCollection p = new System.Collections.Specialized.NameValueCollection();
+            p.Add("id", ReportNumber.Text);
+            Snowball.Common.PostSubmitter submitter = new Snowball.Common.PostSubmitter(
+                "http://www.bloodlinesresurgence.com/error_reports/get.php", p);
+            string content = submitter.Post();
+            UTF8Encoding encoding = new UTF8Encoding();
+            byte[] content_b = encoding.GetBytes(content);
+            System.IO.MemoryStream input = new System.IO.MemoryStream(content_b);
+            System.IO.MemoryStream output = new System.IO.MemoryStream();
+            System.IO.Compression.DeflateStream deflate = new System.IO.Compression.DeflateStream(input, System.IO.Compression.CompressionMode.Decompress);
+            byte[] buffer = new byte[4096];
+            int numRead = 0;
+            while ((numRead = deflate.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                output.Write(buffer, 0, numRead);
+            }
+            output.Seek(0, System.IO.SeekOrigin.Begin);
+            byte[] final_buffer = new byte[output.Length];
+            string final = encoding.GetString(final_buffer);
+
+            Report.Text = final;
+        }
+    }
+}
