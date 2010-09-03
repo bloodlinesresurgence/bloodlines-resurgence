659c783d4f8fbbeb8dc939f4202bb980369a2f9c
Daedalus Raistlin <daedalus.raistlin@gmail.com>
2010-09-03 23:53:21 +1000

    Bringing back the Readme / Welcome file

---
+++ tools/installer-legacy/ResurgenceTools/WizardPages/Readme.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Resurgence.WizardPages
+{
+    partial class Readme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Understand = new System.Windows.Forms.CheckBox();
+            this.Document = new System.Windows.Forms.RichTextBox();
+            this.ContentPanel.SuspendLayout();
+            this.ControlPanel.SuspendLayout();
+            this.extendedPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.Controls.Add(this.Document);
+            this.ContentPanel.Controls.Add(this.Understand);
+            this.ContentPanel.Controls.SetChildIndex(this.Understand, 0);
+            this.ContentPanel.Controls.SetChildIndex(this.Document, 0);
+            // 
+            // NextButton
+            // 
+            this.NextButton.Enabled = false;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // BackButton
+            // 
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // Understand
+            // 
+            this.Understand.AutoSize = true;
+            this.Understand.Location = new System.Drawing.Point(12, 218);
+            this.Understand.Name = "Understand";
+            this.Understand.Size = new System.Drawing.Size(187, 17);
+            this.Understand.TabIndex = 4;
+            this.Understand.Text = "I understand the above conditions";
+            this.Understand.UseVisualStyleBackColor = true;
+            this.Understand.CheckedChanged += new System.EventHandler(this.Understand_CheckedChanged);
+            // 
+            // Document
+            // 
+            this.Document.Location = new System.Drawing.Point(13, 11);
+            this.Document.Name = "Document";
+            this.Document.ReadOnly = true;
+            this.Document.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.Document.Size = new System.Drawing.Size(469, 196);
+            this.Document.TabIndex = 5;
+            this.Document.Text = "";
+            this.Document.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.Document_LinkClicked);
+            // 
+            // Readme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 350);
+            this.Name = "Readme";
+            this.Text = "Readme";
+            this.ContentPanel.ResumeLayout(false);
+            this.ContentPanel.PerformLayout();
+            this.ControlPanel.ResumeLayout(false);
+            this.ControlPanel.PerformLayout();
+            this.extendedPanel3.ResumeLayout(false);
+            this.extendedPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Document;
+        private System.Windows.Forms.CheckBox Understand;
+    }
+}
---
+++ tools/installer-legacy/ResurgenceTools/WizardPages/Readme.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ResurgenceLib;
+
+namespace Resurgence.WizardPages
+{
+    public partial class Readme : ResurgenceWizardPage
+    {
+        public Readme()
+        {
+            InitializeComponent();
+        }
+
+        public Readme(TranslationProvider Provider)
+            : base(Provider)
+        {
+        }
+
+        protected override void DoInitializeComponent()
+        {
+            base.DoInitializeComponent();
+            InitializeComponent();
+
+            // Load the welcome document for the specified language.
+            string doc = Program.Settings.WelcomeDocument;
+            if (System.IO.File.Exists(doc))
+                Document.LoadFile(doc);
+            else
+                Document.Text = String.Format(Translate("!WelcomeDocNotFound"), doc);
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Program.NextForm = new SelectPaths(TranslationProvider);
+            Close();
+        }
+
+        private void Understand_CheckedChanged(object sender, EventArgs e)
+        {
+            NextButton.Enabled = Understand.Checked;
+        }
+
+        private void Document_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            Program.NextForm = new Welcome(TranslationProvider);
+            Close();
+        }
+    }
+}
