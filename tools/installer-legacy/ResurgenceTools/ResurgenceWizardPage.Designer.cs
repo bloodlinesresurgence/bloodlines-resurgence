5a511a1b05ebd5ae3095585ce68a8efc1dd17f0a
Julian Thatcher <julian@noblesamurai.com>
2010-09-03 18:18:42 +1000

    Use Bloodlines icon

--- tools/installer-legacy/ResurgenceTools/ResurgenceWizardPage.Designer.cs
+++ tools/installer-legacy/ResurgenceTools/ResurgenceWizardPage.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResurgenceWizardPage));
             this.CheckForUpdates = new System.Windows.Forms.Timer(this.components);
             this.UpdateCheck = new System.Windows.Forms.LinkLabel();
             this.SendErrorReport = new System.Windows.Forms.Button();
@@ -96,6 +97,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(494, 350);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ResurgenceWizardPage";
             this.Text = "ResurgenceWizardPage";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ResurgenceWizardPage_FormClosed);
